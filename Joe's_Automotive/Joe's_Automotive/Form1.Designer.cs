4ab631583428920ebb40a0069c14ec374bb0ea2b
ThomasClark1 <43045242+ThomasClark1@users.noreply.github.com>
2019-03-25 14:18:27 -0400

    f

--- Joe's_Automotive/Joe's_Automotive/Form1.Designer.cs
+++ Joe's_Automotive/Joe's_Automotive/Form1.Designer.cs
@@ -28,20 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCustomersEdit = new System.Windows.Forms.Button();
+            this.btnCustomersView = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInvoicesEdit = new System.Windows.Forms.Button();
+            this.btnInvoicesView = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnVehiclesEdit = new System.Windows.Forms.Button();
+            this.btnVehiclesView = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPartsEdit = new System.Windows.Forms.Button();
+            this.btnPartsView = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(263, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customers";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCustomersEdit
+            // 
+            this.btnCustomersEdit.Location = new System.Drawing.Point(263, 33);
+            this.btnCustomersEdit.Name = "btnCustomersEdit";
+            this.btnCustomersEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnCustomersEdit.TabIndex = 1;
+            this.btnCustomersEdit.Text = "Edit";
+            this.btnCustomersEdit.UseVisualStyleBackColor = true;
+            this.btnCustomersEdit.Click += new System.EventHandler(this.btnCustomersEdit_Click);
+            // 
+            // btnCustomersView
+            // 
+            this.btnCustomersView.Location = new System.Drawing.Point(344, 33);
+            this.btnCustomersView.Name = "btnCustomersView";
+            this.btnCustomersView.Size = new System.Drawing.Size(75, 23);
+            this.btnCustomersView.TabIndex = 2;
+            this.btnCustomersView.Text = "View";
+            this.btnCustomersView.UseVisualStyleBackColor = true;
+            this.btnCustomersView.Click += new System.EventHandler(this.btnCustomersView_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(263, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Invoices";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnInvoicesEdit
+            // 
+            this.btnInvoicesEdit.Location = new System.Drawing.Point(263, 82);
+            this.btnInvoicesEdit.Name = "btnInvoicesEdit";
+            this.btnInvoicesEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnInvoicesEdit.TabIndex = 4;
+            this.btnInvoicesEdit.Text = "Edit";
+            this.btnInvoicesEdit.UseVisualStyleBackColor = true;
+            this.btnInvoicesEdit.Click += new System.EventHandler(this.btnInvoicesEdit_Click);
+            // 
+            // btnInvoicesView
+            // 
+            this.btnInvoicesView.Location = new System.Drawing.Point(344, 82);
+            this.btnInvoicesView.Name = "btnInvoicesView";
+            this.btnInvoicesView.Size = new System.Drawing.Size(75, 23);
+            this.btnInvoicesView.TabIndex = 5;
+            this.btnInvoicesView.Text = "View";
+            this.btnInvoicesView.UseVisualStyleBackColor = true;
+            this.btnInvoicesView.Click += new System.EventHandler(this.btnInvoicesView_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(263, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Vehicles";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnVehiclesEdit
+            // 
+            this.btnVehiclesEdit.Location = new System.Drawing.Point(263, 131);
+            this.btnVehiclesEdit.Name = "btnVehiclesEdit";
+            this.btnVehiclesEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnVehiclesEdit.TabIndex = 7;
+            this.btnVehiclesEdit.Text = "Edit";
+            this.btnVehiclesEdit.UseVisualStyleBackColor = true;
+            this.btnVehiclesEdit.Click += new System.EventHandler(this.btnVehiclesEdit_Click);
+            // 
+            // btnVehiclesView
+            // 
+            this.btnVehiclesView.Location = new System.Drawing.Point(344, 131);
+            this.btnVehiclesView.Name = "btnVehiclesView";
+            this.btnVehiclesView.Size = new System.Drawing.Size(75, 23);
+            this.btnVehiclesView.TabIndex = 8;
+            this.btnVehiclesView.Text = "View";
+            this.btnVehiclesView.UseVisualStyleBackColor = true;
+            this.btnVehiclesView.Click += new System.EventHandler(this.btnVehiclesView_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(263, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Parts";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPartsEdit
+            // 
+            this.btnPartsEdit.Location = new System.Drawing.Point(263, 180);
+            this.btnPartsEdit.Name = "btnPartsEdit";
+            this.btnPartsEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnPartsEdit.TabIndex = 10;
+            this.btnPartsEdit.Text = "Edit";
+            this.btnPartsEdit.UseVisualStyleBackColor = true;
+            this.btnPartsEdit.Click += new System.EventHandler(this.btnPartsEdit_Click);
+            // 
+            // btnPartsView
+            // 
+            this.btnPartsView.Location = new System.Drawing.Point(344, 180);
+            this.btnPartsView.Name = "btnPartsView";
+            this.btnPartsView.Size = new System.Drawing.Size(75, 23);
+            this.btnPartsView.TabIndex = 11;
+            this.btnPartsView.Text = "View";
+            this.btnPartsView.UseVisualStyleBackColor = true;
+            this.btnPartsView.Click += new System.EventHandler(this.btnPartsView_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(83, 182);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 217);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPartsView);
+            this.Controls.Add(this.btnPartsEdit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnVehiclesView);
+            this.Controls.Add(this.btnVehiclesEdit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnInvoicesView);
+            this.Controls.Add(this.btnInvoicesEdit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCustomersView);
+            this.Controls.Add(this.btnCustomersEdit);
+            this.Controls.Add(this.label1);
             this.Name = "MainForm";
-            this.Text = "Main Screen";
+            this.Text = "Joe\'s Automotive CRM";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCustomersEdit;
+        private System.Windows.Forms.Button btnCustomersView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInvoicesEdit;
+        private System.Windows.Forms.Button btnInvoicesView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnVehiclesEdit;
+        private System.Windows.Forms.Button btnVehiclesView;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPartsEdit;
+        private System.Windows.Forms.Button btnPartsView;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Joe's_Automotive/Joe's_Automotive/Form1.cs
+++ Joe's_Automotive/Joe's_Automotive/Form1.cs
@@ -16,5 +16,63 @@ namespace Final_Exam
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCustomersEdit_Click(object sender, EventArgs e)
+        {
+            CustomerEdit customerEdit = new CustomerEdit();
+            customerEdit.Show();
+        }
+
+        private void btnCustomersView_Click(object sender, EventArgs e)
+        {
+            CustomerView customerView = new CustomerView();
+            customerView.Show();
+        }
+
+        private void btnInvoicesEdit_Click(object sender, EventArgs e)
+        {
+            InvoicesEdit invoicesEdit = new InvoicesEdit();
+            invoicesEdit.Show();
+        }
+
+        private void btnInvoicesView_Click(object sender, EventArgs e)
+        {
+            InvoicesView invoicesView = new InvoicesView();
+            invoicesView.Show();
+        }
+
+        private void btnVehiclesEdit_Click(object sender, EventArgs e)
+        {
+            VehiclesEdit vehiclesEdit = new VehiclesEdit();
+            vehiclesEdit.Show();
+        }
+
+        private void btnVehiclesView_Click(object sender, EventArgs e)
+        {
+            VehicleView vehiclesView = new VehicleView();
+            vehiclesView.Show();
+        }
+
+        private void btnPartsEdit_Click(object sender, EventArgs e)
+        {
+            PartsEdit partsEdit = new PartsEdit();
+            partsEdit.Show();
+        }
+
+        private void btnPartsView_Click(object sender, EventArgs e)
+        {
+            PartsView partsView = new PartsView();
+            partsView.Show();
+        }
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
