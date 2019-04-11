1463fa25dcd6eb9393f78c0c0555b8486a959992
ThomasClark1 <43045242+ThomasClark1@users.noreply.github.com>
2019-04-11 11:57:26 -0400

    f

--- Joe's_Automotive/Joe's_Automotive/InvoicesForm.Designer.cs
+++ Joe's_Automotive/Joe's_Automotive/InvoicesForm.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnCreateInvoice = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnCreateInvoice
+            // 
+            this.BtnCreateInvoice.Location = new System.Drawing.Point(13, 13);
+            this.BtnCreateInvoice.Name = "BtnCreateInvoice";
+            this.BtnCreateInvoice.Size = new System.Drawing.Size(125, 30);
+            this.BtnCreateInvoice.TabIndex = 0;
+            this.BtnCreateInvoice.Text = "Create Invoice";
+            this.BtnCreateInvoice.UseVisualStyleBackColor = true;
             // 
             // InvoicesEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnCreateInvoice);
             this.Name = "InvoicesEdit";
             this.Text = "Edit Invoices";
             this.ResumeLayout(false);
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnCreateInvoice;
     }
 }
